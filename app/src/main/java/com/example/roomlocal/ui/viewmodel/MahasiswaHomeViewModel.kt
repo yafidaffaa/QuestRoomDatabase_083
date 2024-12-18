0488cd55d0e3340a0c2bf19bed8f952db6ebf13d
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-18 22:31:13 +0700

    membuat fungsi untuk mengambil data, memasukan data ke variable, dan menyimpan stateFlow

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/MahasiswaHomeViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/MahasiswaHomeViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.roomlocal.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomlocal.data.entity.Mahasiswa
+import com.example.roomlocal.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 data class HomeUIState(
     val listMhs: List<Mahasiswa> = listOf(),
@@ -8,3 +19,36 @@ data class HomeUIState(
     val isError: Boolean = false,
     val errorMessage: String = ""
 )
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUIState: StateFlow<HomeUIState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUIState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUIState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUIState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUIState(
+                isLoading = true,
+            )
+        )
+}
