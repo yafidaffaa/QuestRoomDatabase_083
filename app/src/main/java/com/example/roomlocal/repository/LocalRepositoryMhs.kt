c35070c6d4e8710215358a2310261520392def12
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-18 22:06:55 +0700

    menambahkan implementasi pada LocalRepositoryMhs untuk operasi CRUD Mahasiswa

--- app/src/main/java/com/example/roomlocal/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocal/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.roomlocal.repository
 
 import com.example.roomlocal.data.dao.MahasiswaDao
 import com.example.roomlocal.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,5 +10,21 @@ class LocalRepositoryMhs (
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
 
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMhs()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa?> {
+        return mahasiswaDao.getMhs(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMhs(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMhs(mahasiswa)
     }
 }
