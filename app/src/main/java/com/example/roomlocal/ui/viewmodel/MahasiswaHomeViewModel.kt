a5443caa812e1823c89f9fbd5791c6e99c6c6bc1
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-18 22:21:54 +0700

    menambahkan data class untuk menangani kondisi tampilan home

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/MahasiswaHomeViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/MahasiswaHomeViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.roomlocal.ui.viewmodel
 
+import com.example.roomlocal.data.entity.Mahasiswa
+
+data class HomeUIState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
