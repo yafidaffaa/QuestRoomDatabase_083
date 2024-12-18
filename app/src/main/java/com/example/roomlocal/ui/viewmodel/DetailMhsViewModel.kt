38592469f837f22544ecf33bf1dd529a22a04412
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-18 22:45:56 +0700

    menambahkan event untuk memasukan value ke variabel

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,2 +1,16 @@
 package com.example.roomlocal.ui.viewmodel
 
+import com.example.roomlocal.data.entity.Mahasiswa
+
+
+
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
