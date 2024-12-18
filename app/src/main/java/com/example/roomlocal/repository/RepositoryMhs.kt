9dd81bd6622886e4d3e8b1b165f5b30823027b18
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-18 22:04:45 +0700

    menambahkan metode pada interface RepositoryMhs

--- app/src/main/java/com/example/roomlocal/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocal/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.roomlocal.repository
 
 import com.example.roomlocal.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa?>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
