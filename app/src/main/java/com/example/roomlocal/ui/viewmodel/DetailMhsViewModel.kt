89cb23b29f0b541e59e17bad85d0cf207b9515ff
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 03:25:42 +0700

    Menambahkan kelas DetailUIState untuk mengelola state MahasiswaEvent di UI

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,7 +2,18 @@ package com.example.roomlocal.ui.viewmodel
 
 import com.example.roomlocal.data.entity.Mahasiswa
 
+data class DetailUIState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
 
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
 
 fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
     return MahasiswaEvent(
