058f11d8c0154e67613f9962f91cd2592d8ffcfb
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 03:30:40 +0700

    Menambahkan DetailMhsViewModel untuk mengelola UI state

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,6 +1,56 @@
 package com.example.roomlocal.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomlocal.data.entity.Mahasiswa
+import com.example.roomlocal.repository.RepositoryMhs
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+): ViewModel(){
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUIState: StateFlow<DetailUIState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUIState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .catch {
+            emit(
+                DetailUIState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?:"Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUIState(
+                isLoading = true,
+            )
+        )
+    fun deleteMhs(){
+        detailUIState.value.detailUiEvent.toMahasiswaEntity().let{
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
+}
 
 data class DetailUIState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
