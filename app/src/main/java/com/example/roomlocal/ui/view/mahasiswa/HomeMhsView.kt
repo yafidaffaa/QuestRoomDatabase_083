2d3e7f38e2364b1f21563e037c923de11e0f7d34
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 04:33:08 +0700

    Menambahkan fungsi untuk menampilkan daftar mahasiswa dengan status loading, error, atau kosong.

--- app/src/main/java/com/example/roomlocal/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/roomlocal/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,8 +1,10 @@
 package com.example.roomlocal.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,17 +14,78 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.roomlocal.data.entity.Mahasiswa
+import com.example.roomlocal.ui.viewmodel.HomeUIState
+import kotlinx.coroutines.launch
 
 
+
+@Composable
+fun BodyHomeMhsView(
+    homeUIState: HomeUIState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when{
+        homeUIState.isLoading -> {
+            Box(
+                modifier=modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUIState.isError -> {
+            LaunchedEffect(homeUIState.errorMessage) {
+                homeUIState.errorMessage?.let {message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        homeUIState.listMhs.isEmpty() -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Tidak ada data mahasiswa.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            ListMahasiswa(
+                listMhs = homeUIState.listMhs,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 
 @Composable
 fun ListMahasiswa(
