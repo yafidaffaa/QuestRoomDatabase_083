11b02e787ac27b24ca1e36dddfe283146ce703be
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 04:06:04 +0700

    Menambahkan fungsi untuk menampilkan detail mahasiswa dengan judul dan isi.

--- app/src/main/java/com/example/roomlocal/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/roomlocal/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,10 +1,38 @@
 package com.example.roomlocal.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(text = "$judul",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
 
 @Composable
 private fun DeleteConfirmationDialog(
