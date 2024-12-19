bb331292b78b736bbd10628080e967fa5bbb03ca
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 03:55:12 +0700

    Menambahkan objek DestinasiHome sebagai alamat navigasi untuk halaman home.

--- app/src/main/java/com/example/roomlocal/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/roomlocal/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,7 @@ package com.example.roomlocal.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi {
+    override val route: String = "home"
+}
