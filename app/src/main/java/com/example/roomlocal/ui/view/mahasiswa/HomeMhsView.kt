bb934f4004206765a88af21ab5269099c8000db2
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 04:29:10 +0700

    Menambahkan fungsi untuk menampilkan daftar mahasiswa dalam bentuk list yang dapat diklik.

--- app/src/main/java/com/example/roomlocal/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/roomlocal/ui/view/mahasiswa/HomeMhsView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
@@ -21,6 +23,27 @@ import androidx.compose.ui.unit.sp
 import com.example.roomlocal.data.entity.Mahasiswa
 
 
+
+@Composable
+fun ListMahasiswa(
+    listMhs: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+){
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = listMhs,
+            itemContent = {mhs ->
+                CardMhs(
+                    mhs=mhs,
+                    onClick = {onClick(mhs.nim)}
+                )
+            }
+        )
+    }
+}
 
 @Composable
 fun CardMhs(
