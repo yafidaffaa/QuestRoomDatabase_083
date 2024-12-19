2f967aa975387d9ae99c3d9972c740093cb64754
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 03:35:09 +0700

    Menambahkan UpdateMhsViewModel untuk mengelola UI state pembaruan data mahasiswa.

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,2 +1,36 @@
 package com.example.roomlocal.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomlocal.data.entity.Mahasiswa
+import com.example.roomlocal.repository.RepositoryMhs
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+): ViewModel(){
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState=repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
+
+}
+
+fun Mahasiswa.toUIStateMhs():MhsUIState = MhsUIState(
+    mahasiswaEvent = this.toDetailUiEvent(),
+)
