84d88e473df4fce36f9ccdf06ed195100e25b726
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 04:37:03 +0700

    Menambahkan fungsi untuk menampilkan daftar mahasiswa dengan tombol tambah dan navigasi detail.

--- app/src/main/java/com/example/roomlocal/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/roomlocal/ui/view/mahasiswa/HomeMhsView.kt
@@ -10,16 +10,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -27,11 +33,54 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomlocal.data.entity.Mahasiswa
+import com.example.roomlocal.ui.costumwidget.TopAppBar
+import com.example.roomlocal.ui.viewmodel.HomeMhsViewModel
 import com.example.roomlocal.ui.viewmodel.HomeUIState
+import com.example.roomlocal.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
+@Composable
+fun HomeMhsView(
+    viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Mahasiswa",
+                showBackButton = false,
+                onBack = { },
+                //modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes. medium,
+                modifier = Modifier.padding(16.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mahasiswa",
+                )
+            }
+        }
+    ) {innerPadding ->
+        val homeUIState by viewModel.homeUIState.collectAsState()
 
+        BodyHomeMhsView(
+            homeUIState = homeUIState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
 
 @Composable
 fun BodyHomeMhsView(
