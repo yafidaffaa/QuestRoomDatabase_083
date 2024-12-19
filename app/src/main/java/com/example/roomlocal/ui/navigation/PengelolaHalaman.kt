01a8b514e113ca3a9a4ff87427eaeed5d6d20512
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-20 04:45:50 +0700

    Menambahkan navigasi ke HomeMhsView

--- app/src/main/java/com/example/roomlocal/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomlocal/ui/navigation/PengelolaHalaman.kt
@@ -9,6 +9,7 @@ import androidx.navigation.compose.composable
 
 import androidx.navigation.compose.rememberNavController
 import com.example.roomlocal.ui.view.mahasiswa.DestinasiInsert
+import com.example.roomlocal.ui.view.mahasiswa.HomeMhsView
 import com.example.roomlocal.ui.view.mahasiswa.InsertMhsView
 
 @Composable
@@ -20,6 +21,23 @@ fun PengelolaHalaman(
         navController = navController,
         startDestination = DestinasiInsert.route
     ) {
+        composable(
+            route = DestinasiHome.route
+        ) {
+            HomeMhsView(
+                onDetailClick = {nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println(
+                        "PengelolaHalaman: nim = $nim"
+                    )
+                },
+                onAddMhs = {
+                    navController.navigate((DestinasiInsert.route))
+                },
+                modifier = modifier
+            )
+        }
+
         composable(
             route = DestinasiInsert.route
         ) {
